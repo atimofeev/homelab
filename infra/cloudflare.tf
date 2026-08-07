fc40750ff34bbc1e13a1063783f9e79f6d831016
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2026-08-07 13:04:33 +0200

    feat(cloudflare): add R2 storage usage alert
    Closes #33

--- infra/cloudflare.tf
+++ infra/cloudflare.tf
@@ -5,6 +5,11 @@ data "cloudflare_zone" "this" {
   }
 }
 
+data "cloudflare_account_members" "accepted" {
+  account_id = data.cloudflare_zone.this.account.id
+  status     = "accepted"
+}
+
 resource "cloudflare_dns_record" "routeros_ddns" {
   zone_id = data.cloudflare_zone.this.id
   name    = "homelab-ingress.${var.domain}"
@@ -15,3 +20,22 @@ resource "cloudflare_dns_record" "routeros_ddns" {
   proxied = false
 }
 
+resource "cloudflare_notification_policy" "r2_storage" {
+  account_id = data.cloudflare_zone.this.account.id
+  name       = "R2 storage approaching free-tier limit"
+  alert_type = "billing_usage_alert"
+  enabled    = true
+
+  mechanisms = {
+    email = [
+      for member in data.cloudflare_account_members.accepted.result : {
+        id = member.email
+      }
+    ]
+  }
+
+  filters = {
+    product = ["r2_storage"]
+    limit   = [tostring(9 * 1024 * 1024 * 1024)]
+  }
+}
